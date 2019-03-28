8e8830ac99eebb73fd33435132376298fe26346b
RickyAS <raustin@student.ciputra.ac.id>
2019-03-28 16:46:41 +0700

    Initial commit

---
+++ app/src/main/java/com/testcom/attendon/Register.java
@@ -0,0 +1,65 @@
+package com.testcom.attendon;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Register extends AppCompatActivity {
+EditText register_firstname, register_lastname, register_username, register_email, register_password;
+TextView register_signin;
+Button register_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        register_firstname = findViewById(R.id.register_firstname);
+        register_lastname = findViewById(R.id.register_lastname);
+        register_username = findViewById(R.id.register_username);
+        register_email = findViewById(R.id.register_email);
+        register_password = findViewById(R.id.register_password);
+        register_button = findViewById(R.id.register_button);
+        register_signin = findViewById(R.id.register_signin);
+
+
+        SpannableString ss = new SpannableString("Already have an AttendOn account?");
+
+        ClickableSpan signin = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+                Intent i = new Intent(Register.this,SignIn.class);
+                startActivity(i);
+            }
+
+            @Override
+            public void updateDrawState(@NonNull TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setColor(Color.BLUE);
+                ds.setUnderlineText(false);
+            }
+        };
+        ss.setSpan(signin,1,33, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        register_signin.setText(ss);
+        register_signin.setMovementMethod(LinkMovementMethod.getInstance());
+
+          register_button.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View v) {
+                  Intent i = new Intent(Register.this,SignIn.class);
+                  startActivity(i);
+              }
+          });
+    }
+}
---
+++ app/src/main/java/com/testcom/attendon/SignIn.java
@@ -0,0 +1,64 @@
+package com.testcom.attendon;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SignIn extends AppCompatActivity {
+EditText sign_username, sign_userpassword;
+TextView sign_signup;
+Button sign_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+
+        sign_username = findViewById(R.id.sign_username);
+        sign_userpassword = findViewById(R.id.sign_userpassword);
+        sign_signup = findViewById(R.id.sign_signup);
+        sign_button =  findViewById(R.id.sign_button);
+
+
+        SpannableString ss = new SpannableString("Don't have an account ?");
+
+        ClickableSpan signup = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+                Intent i = new Intent(SignIn.this,Register.class);
+                startActivity(i);
+            }
+
+            @Override
+            public void updateDrawState(@NonNull TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setColor(Color.BLUE);
+                ds.setUnderlineText(false);
+            }
+        };
+
+        ss.setSpan(signup,1,24, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        sign_signup.setText(ss);
+        sign_signup.setMovementMethod(LinkMovementMethod.getInstance());
+
+        sign_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}

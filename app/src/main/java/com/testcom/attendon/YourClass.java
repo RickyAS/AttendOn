cbe07d2928afd1a0f6cac3f5fce5da39be8c084c
RickyAS <raustin@student.ciputra.ac.id>
2019-04-01 10:48:16 +0700

    segmented button

--- app/src/main/java/com/testcom/attendon/YourClass.java
+++ app/src/main/java/com/testcom/attendon/YourClass.java
@@ -4,13 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
 
 import java.util.ArrayList;
+
+
 
 public class YourClass extends AppCompatActivity {
     private RecyclerView recyclerView;
     private MahasiswaAdapter adapter;
     private ArrayList<Mahasiswa> mahasiswaArrayList;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,7 @@ public class YourClass extends AppCompatActivity {
         setContentView(R.layout.activity_your_class);
 
         addData();
+
 
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
